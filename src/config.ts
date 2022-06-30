98c243f91bd42f134ae28160c39d3f20d2f98ffc
Vatsal Singh <vatsalsinghkv@gmail.com>
2022-06-30 23:26:42 +0530

    fix: apikey

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'jiosaavn-api-v3.vercel.app/';
 export const BASE_URL = 'https://shazam.p.rapidapi.com/';
-export const API_KEY = process.env?.REACT_APP_API_KEY ?? '';
+export const API_KEY = process.env.REACT_APP_API_KEY ?? '';
